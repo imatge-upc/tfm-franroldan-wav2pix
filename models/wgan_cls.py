afe27e7124aafbb011961b379c59550d70dc0087
Naveen V <velnaveen99@gmail.com>
2019-10-03 19:33:36 +0530

    Update wgan_cls.py

--- models/wgan_cls.py
+++ models/wgan_cls.py
@@ -56,6 +56,7 @@ class generator(nn.Module):
     def forward(self, embed_vector, z, project=True):
         if self.dataset == 'youtubers' and not project:
             padding = Variable(torch.cuda.FloatTensor(embed_vector.data.shape[0], self.projected_embed_dim-62).fill_(0).float()).cuda()
+            #padding = Variable(torch.FloatTensor(embed_vector.data.shape[0], self.projected_embed_dim-62).fill_(0).float())
             projected_embed = torch.cat([embed_vector, padding], 1).unsqueeze(2).unsqueeze(3)
         else:
             projected_embed = self.projection(embed_vector).unsqueeze(2).unsqueeze(3)
@@ -124,6 +125,7 @@ class discriminator(nn.Module):
         if self.dataset_name == 'youtubers' and not project:
             if concat:
                 padding = Variable(torch.cuda.FloatTensor(embed.data.shape[0], 64 - embed.data.shape[1]).fill_(0).float()).cuda()
+                padding = Variable(torch.FloatTensor(embed.data.shape[0], 64 - embed.data.shape[1]).fill_(0).float())
                 embed_vector = torch.cat([embed, padding], 1).unsqueeze(2).unsqueeze(3).repeat(1, 1, 4, 4)
                 x = torch.cat([x_intermediate, embed_vector], 1)
             else:
